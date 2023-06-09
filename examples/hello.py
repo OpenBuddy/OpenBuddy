6c238a50cce7a27ccdcd142feb1d8c07580728be
44670 <44670@users.noreply.github.com>
2023-06-09 16:59:42 +0800

    update hello.py

--- examples/hello.py
+++ examples/hello.py
@@ -1,9 +1,12 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 
-# You need to download the model into the `examples` directory
-model_path = './openbuddy-falcon-7b-v1.5-fp16/'
-model = AutoModelForCausalLM.from_pretrained(model_path, device_map="auto", trust_remote_code=True)
+model_path = 'OpenBuddy/openbuddy-falcon-7b-v5-fp16'
+model = AutoModelForCausalLM.from_pretrained(
+    model_path, 
+    device_map="auto", 
+    trust_remote_code=True, 
+    torch_dtype=torch.float16)
 tokenizer = AutoTokenizer.from_pretrained(model_path)
 
 with open('../system.prompt', 'r', encoding='utf-8') as f:
@@ -11,5 +14,5 @@ with open('../system.prompt', 'r', encoding='utf-8') as f:
 
 prompt += "\n\nUser: Write a poem about yourself.\nAssistant:"
 input_ids = tokenizer.encode(prompt, return_tensors='pt')
-output_ids = model.generate(input_ids, max_new_tokens=100)
+output_ids = model.generate(input_ids=input_ids, max_new_tokens=100)
 print(tokenizer.decode(output_ids[0], skip_special_tokens=True))
