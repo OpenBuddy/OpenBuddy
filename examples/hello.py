7855d73a35f472a4c85ef458786972bd6c217cba
44670 <44670@users.noreply.github.com>
2023-06-09 18:27:39 +0800

    upd example

--- examples/hello.py
+++ examples/hello.py
@@ -13,9 +13,12 @@ with open('../system.prompt', 'r', encoding='utf-8') as f:
     prompt = f.read()
 
 prompt += "\n\nUser: Write a poem about yourself.\nAssistant:"
-input_ids = tokenizer.encode(prompt, return_tensors='pt')
+input_ids = tokenizer.encode(prompt, return_tensors='pt').to('cuda')
 
 with torch.no_grad():
-    output_ids = model.generate(input_ids=input_ids, max_new_tokens=100)
+    output_ids = model.generate(
+        input_ids=input_ids, 
+        max_new_tokens=100, 
+        eos_token_id=tokenizer.eos_token_id)
 
 print(tokenizer.decode(output_ids[0], skip_special_tokens=True))
